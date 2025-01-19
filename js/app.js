20a967122991e23ab3aa386220b98337952c0179
abdulvahob0714@gmail.com <dilbarxonxiziraliyeva@gmail.com>
2025-01-19 15:50:55 +0500

    add

--- js/app.js
+++ js/app.js
@@ -10,7 +10,20 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
     loop: true,
-    
+    breakpoints: {
+        320: { // Kichik ekranlar (mobil)
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        640: { // Kichikroq planshet
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: { // Katta ekranlar (desktop)
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
 });
 
 
@@ -31,8 +44,11 @@ let hamburgerClose = document.querySelector(".hamburgerClose")
 
 hamburgerBars.addEventListener('click' , function(){
     hambutgerMenu.classList.add('active')
+    document.body.classList.add("active")
 })
 
 hamburgerClose.addEventListener('click' , function(){
     hambutgerMenu.classList.remove('active')
+    document.body.classList.remove("active")
+
 })
