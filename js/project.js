ae4e6fb16c3ea8b3033e3f1d67542a11d4f1c797
Aziz <azizbek.k@wiztechgroup.com>
2025-01-19 20:19:24 +0500

    fix: prevent errors by using optional chaining

--- js/project.js
+++ js/project.js
@@ -1,7 +1,7 @@
 const filterButtons = document.querySelectorAll(".filter-buttons button");
 const galleryItems = document.querySelectorAll(".gallery .item");
 
-filterButtons.forEach(filterButton => {
+filterButtons?.forEach(filterButton => {
     filterButton.addEventListener("click", () => {
         const filter = filterButton.dataset.filter;
         const isActiveBtn = filterButton.classList.contains('active');
