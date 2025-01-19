91663aefb5bfc761378bb98b77ebabe3ddfbb8d5
abdulvahob0714@gmail.com <dilbarxonxiziraliyeva@gmail.com>
2025-01-20 00:10:31 +0500

    add

--- js/app.js
+++ js/app.js
@@ -38,17 +38,46 @@ scrollBtn?.addEventListener('click', (e) => {
 AOS.init();
 
 
-let hambutgerMenu = document.querySelector(".responsive-menu")
-let hamburgerBars = document.querySelector(".hamburgerBars")
-let hamburgerClose = document.querySelector(".hamburgerClose")
 
-hamburgerBars?.addEventListener('click' , function(){
-    hambutgerMenu.classList.add('active')
-    document.body.classList.add("active")
-})
+if(document.querySelector(".responsive-menu")){
+    let hambutgerMenu = document.querySelector(".responsive-menu")
+    let hamburgerBars = document.querySelector(".hamburgerBars")
+    let hamburgerClose = document.querySelector(".hamburgerClose")
+    
+    hamburgerBars?.addEventListener('click' , function(){
+        hambutgerMenu.classList.add('active')
+        document.body.classList.add("active")
+    })
+    
+    hamburgerClose?.addEventListener('click' , function(){
+        hambutgerMenu.classList.remove('active')
+        document.body.classList.remove("active")
+    
+    })
+}
 
-hamburgerClose?.addEventListener('click' , function(){
-    hambutgerMenu.classList.remove('active')
-    document.body.classList.remove("active")
+let aboutVideoClose = document.querySelector(".about-video-close");
+let aboutVideo = document.querySelector(".about-video");
+let aboutBtn = document.querySelector(".about-btn");
 
-})
+// aboutVideoClose tugmasi bosilganda active klassini olib tashlaymiz
+aboutVideoClose?.addEventListener("click", () => {
+    aboutVideo?.classList.remove("active");
+});
+
+// aboutBtn tugmasi bosilganda active klassini qo'shamiz
+aboutBtn?.addEventListener("click", () => {
+    aboutVideo?.classList.add("active");
+});
+
+// iframe ichida bosilganda hodisani to'xtatamiz
+
+
+// aboutVideo tashqarisiga bosilganda active klassini olib tashlaymiz
+document.addEventListener("click", (event) => {
+    if (!aboutVideo?.contains(event.target) && !aboutBtn?.contains(event.target)) {
+        aboutVideo?.classList.remove("active");
+    }
+});
+
+
